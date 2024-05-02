8d4ccfe474a0bef06d2374150df1668db3620317
Shantralee-58 <lindiwekhumalo833@gmail.com>
2024-05-02 17:09:12 +0000

    updated

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+'''Task 0: Basic annotations - add
+'''
 
 
 def add(a: float, b: float) -> float:
-    """Returns the sum of two floats."""
+    '''Adds two floating-point numbers.
+    '''
     return a + b
