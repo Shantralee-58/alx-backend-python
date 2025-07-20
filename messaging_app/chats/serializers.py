7e955ea143547a1282b6e992e19ebfe807bd8f4e
Shantralee-58 <lindiwekhumalo833@gmail.com>
2025-07-20 12:29:43 +0200

    Serializers file updated

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,15 +1,39 @@
 from rest_framework import serializers
-from .models import Conversation, Message
+from .models import User, Conversation, Message
+from django.contrib.auth.hashers import make_password
 
+# User Serializer
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['user_id', 'username', 'email', 'password', 'first_name', 'last_name', 'phone_number', 'role', 'created_at']
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
+
+
+# Message Serializer
 class MessageSerializer(serializers.ModelSerializer):
+    sender = serializers.CharField(source='sender.username', read_only=True)
+
     class Meta:
         model = Message
-        fields = '__all__'
+        fields = ['message_id', 'sender', 'message_body', 'sent_at']
 
+
+# Conversation Serializer
 class ConversationSerializer(serializers.ModelSerializer):
-    messages = MessageSerializer(many=True, read_only=True)
+    participants = UserSerializer(many=True, read_only=True)
+    messages = serializers.SerializerMethodField()
 
     class Meta:
         model = Conversation
-        fields = ['id', 'title', 'created_at', 'messages']
+        fields = ['conversation_id', 'participants', 'created_at', 'messages']
+
+    def get_messages(self, obj):
+        messages = obj.messages.order_by('sent_at')
+        return MessageSerializer(messages, many=True).data
 
