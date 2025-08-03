96e4d00ecaa7812ee72b881b354dc039861c8130
Idah Lindiwe Khumalo <86086701+Shantralee-58@users.noreply.github.com>
2025-08-03 12:53:14 +0200

    error fixed for task 2

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,3 +1,5 @@
+from django.db.models.signals import post_delete
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import Message, Notification, MessageHistory
@@ -27,3 +29,15 @@ def log_message_edit(sender, instance, **kwargs):
         )
         instance.edited = True
 
+@receiver(post_delete, sender=User)
+def delete_related_on_user_delete(sender, instance, **kwargs):
+    # Delete all messages where user is sender or receiver
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+
+    # Delete all notifications related to this user
+    Notification.objects.filter(user=instance).delete()
+
+    # Delete all message histories related to messages sent or received by this user
+    MessageHistory.objects.filter(message__sender=instance).delete()
+    MessageHistory.objects.filter(message__receiver=instance).delete()
